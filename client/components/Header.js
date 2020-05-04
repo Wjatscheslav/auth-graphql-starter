7d2e02953161a1854b6f6b13ef077805cc51b1ae
Tarasenko <wjatscheslav47@gmail.com>
2020-05-04 23:42:12 +0300

    Added auth buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import getCurrentUser from '../queries/CurrentUser';
+import { Link } from 'react-router';
+import logoutMutation from '../mutations/Logout';
 
 class Header extends Component{
     render() {
-        console.log(this.props.data);
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className='nav-wrapper'>
+                    <Link to='/' className='brand-logo left'>
+                        Home
+                    </Link>
+                    <ul className='right'>
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
+    }
+
+    renderButtons() {
+        const { loading, user } = this.props.data;
+
+        if (loading) {
+            return <div />;
+        }
+        if (user) {
+            return (
+                <li>
+                    <a onClick={this.onLogoutClick.bind(this)}>Logout</a>
+                </li>
+            );
+        } else {
+            return (
+                <div>
+                    <li>
+                        <Link to='/signup'>Signup</Link>
+                    </li>
+                    <li>
+                        <Link to='/login'>Login</Link>
+                    </li>
+                </div>
+            )
+        }
+    }
+
+    onLogoutClick() {
+        this.props.mutate({
+            refetchQueries: [{ query: getCurrentUser }]
+        });
     }
 }
 
-export default graphql(getCurrentUser)(Header);
+export default graphql(logoutMutation)(graphql(getCurrentUser)(Header));
