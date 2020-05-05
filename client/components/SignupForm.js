3801e46ab24f56e71dcab4ac0b5e5ea6d27c9e3e
Tarasenko <wjatscheslav47@gmail.com>
2020-05-05 23:59:21 +0300

    Added callback to signup form

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import { graphql } from 'react-apollo';
 import signupMutation from '../mutations/Signup';
+import getCurrentUser from '../queries/CurrentUser';
 
 class SignupForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { errors: [] };
+    }
+
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{ getCurrentUser }]
+        }).catch(res => {
+            const errors = res.graphQLErrors.map(error => error.message);
+            this.setState({ errors });
+        })
+    }
 
     render() {
         return (
             <div>
                 <h3>Signup</h3>
-                <AuthForm />
+                <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)}/>
             </div>
         );
     }
